0c5e382285b9e06a4f729e21d77515486ede8626
Flavius Pop <popflavius2000@yahoo.ro>
2021-10-20 17:56:33 +0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Pop_Flavius_lab2a
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
